3a17e936ed9031802f5cd8dc870c8b02979e5e1f
Maghil Vannan M <mmaghil2k@gmail.com>
2021-12-12 20:29:13 +0530

    optimized code can now handle -x axis input

--- README.md
+++ README.md
@@ -1,41 +1,7 @@
 Given a list of integers a 2D graph has to be plotted taking 1st, 3rd, 5th, ... numbers as upward slope and 2nd, 4th, ... numbers as downward slope
 
-I have used python list to do it. is there any further optimization possible with python?
-
 input integers :
 >3, 1, 2, 3, 6, 2, 3, 6, 2, 3, 6, 3, 2, 3, 6, 2, 3, 4, 3, 2, 5, 4, 2, 1, 2, 1, 2, 3, 1, 2, 6, 2, 3, 6, 2, 3, 6, 3, 2, 3, 1, 5, 3, 2, 1, 2, 4, 2, 1, 8, 1, 2
-
-my code :
-```
-data = [3, 1, 2, 3, 6, 2, 3, 6, 2, 3, 6, 3, 2, 3, 6, 2, 3, 4, 3, 2, 5, 4, 2, 1, 2, 1, 2, 3, 1, 2, 6, 2, 3, 6, 2, 3, 6, 3, 2, 3, 1, 5, 3, 2, 1, 2, 4, 2, 1, 8, 1, 2]
-length = sum(data)
-print_list=[]
-
-def populate_list():
-    top = -1             #to keep track of max x-axis of list
-    x=0
-    y=0
-    for i in range(0,len(data)):
-        if i % 2==0:
-            for _ in range(0,data[i]):
-                if x>top:
-                    print_list.append([])
-                    top=x
-                    for _ in range(0,length):               #to initialize a new top list
-                        print_list[x].append(" ")
-                print_list[x][y]="/"
-                x+=1
-                y+=1
-        else:
-            for _ in range(0,data[i]):
-                x-=1
-                print_list[x][y]="\\"
-                y+=1
-
-populate_list()
-for i in range(len(print_list)-1,-1,-1):                    #printing from bottom up since generated graph is inverted
-    print(''.join(map(str,print_list[i])))
-```
 
 OUTPUT :
 ```
--- list_solution.py
+++ list_solution.py
@@ -1,28 +1,33 @@
-data = [3, 1, 2, 3, 6, 2, 3, 6, 2, 3, 6, 3, 2, 3, 6, 2, 3, 4, 3, 2, 5, 4, 2, 1, 2, 1, 2, 3, 1, 2, 6, 2, 3, 6, 2, 3, 6, 3, 2, 3, 1, 5, 3, 2, 1, 2, 4, 2, 1, 8, 1, 2]
-length = sum(data)
-print_list=[]
-
-def populate_list():
-    top = -1             #to keep track of max x-axis of list
-    x=0
-    y=0
+def populate_graph(data):
+    graph=[]
+    max_y = sum(data)
+    max_x = -1             #to keep track of max x-axis of list #starting from -1 since no x axis exists as at start
+    x,y = 0,0
+    x_is_zero=False
     for i in range(0,len(data)):
-        if i % 2==0:
-            for _ in range(0,data[i]):
-                if x>top:
-                    print_list.append([])
-                    top=x
-                    for _ in range(0,length):               #to initialize a new top list
-                        print_list[x].append(" ")
-                print_list[x][y]="/"
-                x+=1
+        for _ in range(0,data[i]):
+            if not x_is_zero:
+                if i % 2==0:
+                    if x > max_x:                       #creating a empty list filled with spaces so later we can fill with slashes
+                        max_x+=1
+                        graph.append([" "] * max_y)
+                    graph[x][y]="/"
+                    x+=1
+                else:
+                    if x==0:
+                        print("the graph is getting pushed to -x axis, we currently don't a support for that \n")
+                        x_is_zero = True
+                        print("PRINTING GRAPH BEFORE REACHING -X AXIS")
+                        break
+                    x-=1
+                    graph[x][y]="\\"
                 y+=1
-        else:
-            for _ in range(0,data[i]):
-                x-=1
-                print_list[x][y]="\\"
-                y+=1
+    return(graph[::-1])
 
-populate_list()
-for i in range(len(print_list)-1,-1,-1):                    #printing from bottom up since generated graph is inverted
-    print(''.join(map(str,print_list[i])))
+def graph_printer(graph):
+    for row in graph:
+        print("".join(row))
+
+if __name__ == "__main__":
+    data = [3, 1, 2, 3, 6, 2, 3, 6, 2, 3, 6, 3, 2, 3, 6, 2, 3, 4, 3, 2, 5, 4, 2, 1, 2, 1, 2, 3, 1, 2, 6, 2, 3, 6, 2, 3, 6, 3, 2, 3, 1, 5, 3, 2, 1, 2, 4, 2, 1, 8, 1, 2]
+    graph_printer(populate_graph(data=data))
